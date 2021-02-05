c321008b8834133b40b0df71167e4ab313d82dd8
Alex Yakunin <alex.yakunin@gmail.com>
2021-02-04 17:55:28 -0800

    fix: Run-MultiHost.cmd

--- Run-MultiHost.cmd
+++ Run-MultiHost.cmd
@@ -5,8 +5,8 @@ set ASPNETCORE_ENVIRONMENT=Development
 
 set ASPNETCORE_URLS=http://localhost:5030/
 start cmd /C timeout 5 ^& start http://localhost:5030/"
-start cmd /C dotnet run --no-launch-profile -f net5.0 -p src/Host/Host.csproj
+start cmd /C dotnet run --no-launch-profile -p src/Host/Host.csproj
 
 set ASPNETCORE_URLS=http://localhost:5031/
 start cmd /C timeout 5 ^& start http://localhost:5031/"
-start cmd /C dotnet run --no-launch-profile -f net5.0 -p src/Host/Host.csproj
+start cmd /C dotnet run --no-launch-profile -p src/Host/Host.csproj
