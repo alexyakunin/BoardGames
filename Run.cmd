99bcfe941ac5f694271d23dc729955cb69bade3b
Alex Yakunin <alex.yakunin@gmail.com>
2021-02-11 20:12:49 -0800

    fix: Minor fixes in layouts & messages.

---
+++ Run.cmd
@@ -0,0 +1,8 @@
+@echo off
+dotnet build
+
+set ASPNETCORE_ENVIRONMENT=Development
+
+set ASPNETCORE_URLS=http://localhost:5030/
+start cmd /C timeout 5 ^& start http://localhost:5030/"
+start cmd /C dotnet run --no-launch-profile -p src/Host/Host.csproj
