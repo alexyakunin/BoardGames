995af0163b6d98a9744e3982ece95b563bdf0931
iqmulator <iqmulator@gmail.com>
2021-03-12 14:27:23 +0500

    point game works, need to add scores info

--- src/Abstractions/Games/CardsPoint.cs
+++ src/Abstractions/Games/CardsPoint.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.Extensions.Primitives;
 using Stl.DependencyInjection;
+using Stl.Fusion;
 
 namespace BoardGames.Abstractions.Games
 {
@@ -28,13 +29,17 @@ namespace BoardGames.Abstractions.Games
         Ace = 11
     }
 
-    public class Card
+    public enum Status
+    {
+        Active,
+        Finished
+    }
+
+    public record Card
     {
         public int Id { get; set; }
         public Suits Suit { get; set; }
         public Ranks Rank { get; set; }
-        public int BoardIndex { get; set; } = 100;
-        public int PlayerIndex { get; set; } = 100;
 
         public Card(int id, Suits suit, Ranks rank)
         {
@@ -44,34 +49,19 @@ namespace BoardGames.Abstractions.Games
         }
     }
 
-    public record PointState(CharBoard Board,
-        ImmutableList<Card> Cards,
+    public record PointState(ImmutableList<Card> Cards,
         int MoveIndex = 0,
         int FirstPlayerIndex = 0,
         int PlayersCount = 0)
     {
+        public ImmutableDictionary<int, List<Card>> PlayersCards { get; set; } = ImmutableDictionary<int, List<Card>>.Empty;
+        public ImmutableDictionary<int, Status> Statuses { get; set; } = ImmutableDictionary<int, Status>.Empty;
+        public ImmutableDictionary<int, int> Scores { get; set; } = ImmutableDictionary<int, int>.Empty;
         public int PlayerIndex => (MoveIndex + FirstPlayerIndex) % PlayersCount;
-        public PointState() : this((CharBoard) null!, ImmutableList<Card>.Empty, 0) {}
-    }
 
-    public record PointMove(int Row, int Column) : GameMove
-    {
-        public PointMove() : this(0, 0) { }
-    }
-    
-    [Service, ServiceAlias(typeof(IGameEngine), IsEnumerable = true)]
-    public class PointEngine : GameEngine<PointState, PointMove>
-    {
-        public static int BoardSize { get; } = 6;
+        public PointState() : this((ImmutableList<Card>)null!) { }
 
-        public override string Id => "point";
-        public override string Title => "Point (21)";
-        public override string Icon => "fa-file-powerpoint";
-        public override int MinPlayerCount => 2;
-        public override int MaxPlayerCount => 9;
-        public override bool AutoStart => false;
-
-        public override Game Start(Game game)
+        public PointState(int playersCount) : this((ImmutableList<Card>.Empty))
         {
             var cards = new List<Card>();
             var cardId = 1;
@@ -82,13 +72,41 @@ namespace BoardGames.Abstractions.Games
                     cardId++;
                 }
             }
-            var playerCount = game.Players.Count;
-            var firstPlayerIndex = new Random().Next(0, playerCount);
-            var state = new PointState(CharBoard.Empty(BoardSize),
-                cards.ToImmutableList(),
-                0,
-                firstPlayerIndex,
-                playerCount);
+            Cards = cards.ToImmutableList();
+            var statuses = new Dictionary<int, Status>();
+            var playersCards = new Dictionary<int, List<Card>>();
+            var scores = new Dictionary<int, int>();
+            for (int i = 0; i < playersCount; i++) {
+                statuses.Add(i, Status.Active);
+                playersCards.Add(i, new List<Card>());
+                scores.Add(i, 0);
+            }
+
+            Scores = scores.ToImmutableDictionary();
+            Statuses = statuses.ToImmutableDictionary();
+            PlayersCards = playersCards.ToImmutableDictionary();
+            MoveIndex = 0;
+            FirstPlayerIndex = new Random().Next(0, playersCount);
+            PlayersCount = playersCount;
+        }
+    }
+
+    public record PointMove(bool IsSkip) : GameMove
+    { }
+
+    [Service, ServiceAlias(typeof(IGameEngine), IsEnumerable = true)]
+    public class PointEngine : GameEngine<PointState, PointMove>
+    {
+        public override string Id => "point";
+        public override string Title => "Point (21)";
+        public override string Icon => "fa-file-powerpoint";
+        public override int MinPlayerCount => 2;
+        public override int MaxPlayerCount => 9;
+        public override bool AutoStart => false;
+
+        public override Game Start(Game game)
+        {
+            var state = new PointState(game.Players.Count);
             var player = game.Players[state.PlayerIndex];
             return game with {
                 StateJson = SerializeState(state),
@@ -96,44 +114,175 @@ namespace BoardGames.Abstractions.Games
             };
         }
 
+        public Game Skip(Game game, PointMove move)
+        {
+            if (game.Stage == GameStage.Ended)
+                throw new ApplicationException("Game is ended.");
+            var state = DeserializeState(game.StateJson);
+            var currentPlayerStatus = Status.Finished;
+            var newStatuses = state.Statuses.SetItem(state.PlayerIndex, currentPlayerStatus);
+
+            var newState = state with {
+                Statuses = newStatuses
+            };
+            
+            var newGame = game with {
+                StateJson = SerializeState(newState)
+            };
+            
+            if (newState.Statuses.All(s => s.Value == Status.Finished))
+                return newGame with {
+                    StateJson = SerializeState(newState),
+                    StateMessage = StandardMessages.FinalStandings(newGame),
+                    Stage = GameStage.Ended
+                };
+
+            state = newState with {
+                MoveIndex = GetNextMoveIndex(newState.Statuses, newState.MoveIndex),
+            };
+            var newPlayer = newGame.Players[newState.PlayerIndex];
+            game = newGame with {StateJson = SerializeState(state)};
+            game = game with {
+                StateMessage = StandardMessages.MoveTurn(new AppUser(newPlayer.UserId))
+            };
+            
+            return game;
+        }
+        
         public override Game Move(Game game, PointMove move)
         {
             if (game.Stage == GameStage.Ended)
                 throw new ApplicationException("Game is ended.");
             var state = DeserializeState(game.StateJson);
+            var player = game.Players[state.PlayerIndex];
+            Status currentPlayerStatus;
+            ImmutableDictionary<int, Status> newStatuses = ImmutableDictionary<int, Status>.Empty;
+            var newState = state;
+            var newGame = game;
+            var newPlayer = newGame.Players[newState.PlayerIndex];
+
+            if (move.IsSkip) {
+                currentPlayerStatus = Status.Finished;
+                newStatuses = state.Statuses.SetItem(state.PlayerIndex, currentPlayerStatus);
+
+                newState = state with {
+                    Statuses = newStatuses
+                };
+            
+                newGame = game with {
+                    StateJson = SerializeState(newState)
+                };
+            
+                if (newState.Statuses.All(s => s.Value == Status.Finished))
+                    return newGame with {
+                        StateJson = SerializeState(newState),
+                        StateMessage = StandardMessages.FinalStandings(newGame),
+                        Stage = GameStage.Ended
+                    };
+
+                state = newState with {
+                    MoveIndex = GetNextMoveIndex(newState.Statuses, newState.MoveIndex),
+                };
+                newPlayer = newGame.Players[newState.PlayerIndex];
+                game = newGame with {StateJson = SerializeState(state)};
+                game = game with {
+                    StateMessage = StandardMessages.MoveTurn(new AppUser(newPlayer.UserId))
+                };
+            
+                return game;
+            }
+            
             if (move.PlayerIndex != state.PlayerIndex)
                 throw new ApplicationException("It's another player's turn.");
-            var board = state.Board;
-            var player = game.Players[state.PlayerIndex];
             var cards = state.Cards;
             var card = GetRandomCard(cards);
-            var boardIndex = board.GetCardIndex(move.Row, move.Column);
-            card.BoardIndex = boardIndex;
-            card.PlayerIndex = state.PlayerIndex;
-            cards.SetItem(card.Id, card);
+            var newCards = cards.Remove(card);
+            cards = newCards;
+            var playersCards = state.PlayersCards;
+            var currentCards = playersCards[state.PlayerIndex];
+            currentCards.Add(card);
+            playersCards.SetItem(state.PlayerIndex, currentCards);
+
+            var scores = state.Scores;
+            var playerScores = scores[state.PlayerIndex];
+            var currentPlayerScores = GetPlayerScores(card, playerScores, currentCards.Count);
+            var newScores = scores.SetItem(state.PlayerIndex, currentPlayerScores);
             
-            var nextState = state with {
-                Board = board,
-                MoveIndex = state.MoveIndex + 1,
+            var allStatuses = state.Statuses;
+
+            currentPlayerStatus = CheckOrChangePlayerStatus(currentPlayerScores);
+            if (currentPlayerStatus != state.Statuses[state.PlayerIndex])
+                allStatuses.SetItem(state.PlayerIndex, currentPlayerStatus);
+            newStatuses = currentPlayerStatus == allStatuses[state.PlayerIndex]
+                ? allStatuses
+                : allStatuses.SetItem(state.PlayerIndex, currentPlayerStatus);
+
+            newState = state with {
                 Cards = cards,
-            };
-            var nextPlayer = game.Players[nextState.PlayerIndex];
-            var nextGame = game with {StateJson = SerializeState(nextState)};
-            nextGame = nextGame with {
-                StateMessage = StandardMessages.MoveTurn(new AppUser(nextPlayer.UserId))
+                PlayersCards = playersCards,
+                Scores = newScores,
+                Statuses = newStatuses
             };
             
-            return nextGame;
+            newGame = game with {
+                StateJson = SerializeState(newState)
+            };
+            
+            if (newState.Statuses.All(s => s.Value == Status.Finished))
+                return newGame with {
+                    StateJson = SerializeState(newState),
+                    StateMessage = StandardMessages.FinalStandings(newGame),
+                    Stage = GameStage.Ended
+                };
+
+            state = newState with {
+                MoveIndex = GetNextMoveIndex(newState.Statuses, newState.MoveIndex),
+            };
+            player = newGame.Players[state.PlayerIndex];
+            game = newGame with {StateJson = SerializeState(state)};
+            game = game with {
+                StateMessage = StandardMessages.MoveTurn(new AppUser(player.UserId))
+            };
+            
+            return game;
         }
 
         private Card GetRandomCard(ImmutableList<Card> cards)
         {
             var rnd = new Random();
-            var result = rnd.Next(0, 36);
-            var card = cards[result];
-            if (card.PlayerIndex != 100 && card.BoardIndex != 100)
-                return GetRandomCard(cards);
+            var number = rnd.Next(0, cards.Count);
+            var card = cards[number];
             return card;
+        }
+
+        private int GetPlayerScores(Card card, int playerScores, int cardsCount)
+        {
+            var currentScore = (int)card.Rank + playerScores;
+            if (cardsCount == 2) {
+                if (currentScore == 22) currentScore += 10;
+                else if (currentScore == 21) currentScore += 5;
+            }
+            else if (cardsCount > 2) {
+                if (currentScore == 21) currentScore += 5;
+                else if (currentScore > 21) currentScore = 2;
+            }
+
+            return currentScore;
+        }
+
+        private Status CheckOrChangePlayerStatus(int s)
+        {
+            if (s == 32 || s == 26 || s == 2 || s == 19 || s == 20) return Status.Finished;
+            return Status.Active;
+        }
+
+        private int GetNextMoveIndex(ImmutableDictionary<int, Status> statuses, int moveIndex)
+        {
+            moveIndex += 1;
+            moveIndex %= statuses.Count;
+            if (statuses[moveIndex] == Status.Finished)
+                return GetNextMoveIndex(statuses, moveIndex);
+            return moveIndex;
         }
     }
 }
